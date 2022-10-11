92396dc77e5bb6729a2d2bc679bc0d40b4b03898
gabralvst <gabrialvesteixeira@gmail.com>
2022-10-11 15:12:28 +0200

    Added more inputs

--- Assignment2.py
+++ Assignment2.py
@@ -9,18 +9,22 @@ p0 = 100000 #Pa
 comb_eff = 1
 nozz_eff = 1
 gamma_air = 1.4
+gamma_gas = 1.33
+LHV = 43 * 10**6 #J
+cp_a = 1000
+cp_g = 1150
 p_t2 = p0 * tot_comp_ratio
-print(p_t2)
+
 
 def isentropic_flow(t0, p2, p0, gamma_air):
     isentropic_flow = t0 * (p2 / p0)**((gamma_air-1)/gamma_air)
     return isentropic_flow
 
-
 def T_current(T_previous, isentropic_eff, p_current, p_previous, gamma): #in K
     T_current = T_previous * (1 + 1/isentropic_eff * ((p_current/p_previous)**((gamma-1)/gamma) -1))
     return T_current
 
-#t_t2 = isentropic_flow(t0, p_t2, p0, gamma_air)
+t_t2isentropic = isentropic_flow(t0, p_t2, p0, gamma_air)
 t_t2 = T_current(t0, 0.92, p_t2, p0, gamma_air)
+print(t_t2isentropic)
 print(t_t2)
