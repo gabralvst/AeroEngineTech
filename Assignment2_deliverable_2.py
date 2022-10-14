1c8010dc0f99237a4ab3e7795e693f9467b21769
gabralvst <gabrialvesteixeira@gmail.com>
2022-10-14 16:10:40 +0200

    Added deliverables 2 and 3

---
+++ Assignment2_deliverable_2.py
@@ -0,0 +1,72 @@
+'Assignment 2'
+import numpy as np
+import matplotlib.pyplot as plt
+
+gross_th= 15167 #N
+mdot_a = 23.81  #kg/s
+mdot_f = 0.4267 #kg/s
+tot_comp_ratio = 5.5
+t0 = 288 #k
+p0 = 100000 #Pa
+comb_eff = 1
+nozz_eff = 1
+cp_a = 1000 #J/kg/K
+cp_g = 1150 #J/kg/K     #may be different
+gamma_a = 1.4
+gamma_g = 1.3
+gas_const = 287 #J/kg/K
+
+# Deliverable 2
+A_turbine = 0.037   #m2
+A_nozzle = 0.079  #m2
+Tt1 = 288   #K
+Pt1 = 1e5    #Pa
+Tt3 = 900   #K
+nc = 1  #compressor eff
+nt = 1  #turbine eff
+
+kh = 0.152
+# kh = 1 - (A_turbine/A_nozzle) ** (2*(gamma_g-1)/ (gamma_g+1))
+print('kh', kh)
+
+Tt2 = (1 + cp_g/cp_a * kh * Tt3/Tt1) * Tt1
+comp_T_ratio = (1 + cp_g/cp_a * kh * Tt3/Tt1)
+Pt2 = (1 + cp_g/cp_a * kh * Tt3/Tt1) ** (gamma_a * nc/ (gamma_a-1)) * Pt1   #attention to comp eff
+comp_P_ratio = (1 + cp_g/cp_a * kh * Tt3/Tt1) ** (gamma_a * nc/ (gamma_a-1))
+Pt3 = Pt2 #no losses in combustion chamber
+
+Pt4 = (A_turbine/A_nozzle)**(2*gamma_g / (2*gamma_g - nt*(gamma_g-1))) * Pt3
+turb_P_ratio = (A_turbine/A_nozzle)**(2*gamma_g / (2*gamma_g - nt*(gamma_g-1)))
+Tt4 = (Pt4/Pt3) ** (nt * (gamma_g-1) / gamma_g) * Tt3
+turb_T_ratio = (Pt4/Pt3) ** (nt * (gamma_g-1) / gamma_g)
+chocked_core = Pt4/Pt1
+chocked_limit_core = (1-(1/nozz_eff)*((gamma_g-1)/(gamma_g+1)))**(-gamma_g/(gamma_g-1))
+
+if chocked_core > chocked_limit_core:
+    print('The core nozzle is chocked', chocked_core)
+else:
+    print('The core nozzle is NOT chocked', chocked_core)
+
+mdot_turbine = (1.389 * Pt3 * A_turbine) / (cp_g * Tt3)**0.5
+mdot_nozzle = (1.389 * Pt4 * A_nozzle) / (cp_g * Tt4)**0.5
+
+if chocked_core > chocked_limit_core:
+    T5 = Tt4 * 2/(gamma_g+1)
+    v5 = np.sqrt(gamma_g * gas_const * T5)
+    v5_theirs = (cp_g * T5 * (gamma_g-1)) ** 0.5
+    P4 = Pt4 / (1 + (gamma_a-1)/2 * 1)**(gamma_g/ (gamma_g-1))
+    P5 = Pt4/chocked_limit_core
+    F_core = mdot_turbine * (v5) + A_nozzle * (P5-Pt1)
+else:
+    P5 = Pt1
+    T5 = Tt4 - (Tt4 * nozz_eff * (1 - (Pt1 / Pt4) ** ((gamma_g - 1) / gamma_g)))
+    v5 = np.sqrt(2 * cp_g * (Tt4 - T5))
+    F_core = mdot_turbine * (v5)
+
+
+print('mass flow engine', mdot_turbine, mdot_nozzle)
+
+print('Compressor T ratio', comp_T_ratio, 'Compressor P ratio', comp_P_ratio)
+print('Turbine T ratio', 1/turb_T_ratio, 'Turbine P ratio', 1/turb_P_ratio)
+print('T5', T5, 'v5', v5, 'Thrust', F_core)
+
---
+++ Assignment2_pt3.py
@@ -0,0 +1,72 @@
+'Assignment 2 - Deliverable 3'
+import numpy as np
+gross_th= 15167 #N
+mdot_a = 23.81  #kg/s
+mdot_f = 0.4267 #kg/s
+tot_comp_ratio = 5.5
+comb_eff = 1
+comp_eff = 1
+turb_eff = 1
+nozz_eff = 1
+gamma_air = 1.4
+gamma_gas = 1.3
+gas_const = 287 #J/kg/K
+LHV = 43 * 10**6 #J
+cp_a = 1000
+cp_g = 1150
+h = 10668 #m
+Mach = 0.78
+TIT = 1150 #k
+t_amb = 219
+p_amb = 23911 #Pa
+kh = 0.152
+A_turbine = 0.037   #m2
+A_nozzle = 0.079  #m2
+v0 = Mach*np.sqrt(gamma_air * gas_const * t_amb)
+def total_T_amb(T, gamma, M):
+    total_T_amb = T *(1+ (( gamma - 1) /2) * M **2)
+    return total_T_amb
+
+def total_p_amb(p, gamma, M): #in Pa
+    total_p_amb = p * ((1+ (( gamma - 1) /2) * M **2)) ** (gamma/(gamma-1))
+    return total_p_amb
+
+def isentropic_flow(t0, p2, p0, gamma_air):
+    isentropic_flow = t0 * (p2 / p0)**((gamma_air-1)/gamma_air)
+    return isentropic_flow
+
+def T_current(T_previous, isentropic_eff, p_current, p_previous, gamma): #in K
+    T_current = T_previous * (1 + 1/isentropic_eff * ((p_current/p_previous)**((gamma-1)/gamma) -1))
+    return T_current
+
+t_t_amb = total_T_amb(t_amb, gamma_air, Mach)
+p_t_amb = total_p_amb(p_amb, gamma_air, Mach)
+p_t1 = p_t_amb #assuming no pressure change in the inlet
+t_t1 = t_t_amb
+p_t2 = p_t1 * tot_comp_ratio #p at compressor outlet
+t_t2 = T_current(t_t1, comp_eff, p_t2, p_t1, gamma_air) #t at compressor outlet
+comp_t_ratio = (1 + cp_g/cp_a * kh * (TIT/t_t1))
+p_t3 = p_t2 #assuming no pressure loses in CC
+turb_p_ratio = 1 / ((A_turbine/A_nozzle)**(2*gamma_gas / (2*gamma_gas - turb_eff*(gamma_gas-1)))) #assuming choked turbine and nozzle
+turb_t_ratio = 1 / (turb_p_ratio)**(turb_eff * (gamma_gas-1) / gamma_gas)
+t_t4 = TIT * turb_t_ratio
+p_t4 = p_t3 * turb_p_ratio
+choked_core = p_t4/p_t1
+choked_core_lim = (1-(1/nozz_eff)*((gamma_gas-1)/(gamma_gas+1)))**(-gamma_gas/(gamma_gas-1))
+mdot_engine = (1.389 * p_t3 * A_turbine) / (cp_g * TIT)**0.5
+if choked_core > choked_core_lim:
+    print('The core nozzle is choked', choked_core, choked_core_lim)
+    t_jet = t_t4 * 2 / (gamma_gas + 1)
+    v_jet = np.sqrt(gamma_gas * gas_const * t_jet) # [m/s]
+    p_5 = p_t4 / choked_core_lim
+    F_core = mdot_engine * (v_jet - v0) + A_nozzle * (p_5 - p_amb) #N
+    print('Jet Temperature =', t_jet )
+    print('Jet Velocity =', v_jet)
+    print('Gross Thrust =', F_core)
+else:
+    print('The core nozzle is NOT choked', choked_core)
+
+
+print('Compressor T ratio', comp_t_ratio)
+print('Turbine T ratio', turb_t_ratio, 'Turbine P ratio', turb_p_ratio)
+
