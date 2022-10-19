ab116ada6f68f9812123ab05f441acff63e0eb46
gabralvst <gabrialvesteixeira@gmail.com>
2022-10-19 16:31:18 +0200

    Added static pressure eqs

--- Assignment2_pt4.py
+++ Assignment2_pt4.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 gross_th= 15167 #N
 mdot_a = 23.81  #kg/s
@@ -15,11 +15,13 @@ gas_const = 287 #J/kg/K
 LHV = 43 * 10**6 #J
 cp_a = 1000
 cp_g = 1150
+R = 287
 h = 10668 #m
 Mach = 0.78
 TIT = 1150 #k
 t_amb = 219
 p_amb = 23911 #Pa
+rho_amb = 0.3796
 kh = 0.152
 A_turbine = 0.037   #m2
 A_nozzle = 0.079  #m2
@@ -30,19 +32,57 @@ reaction = 0.5
 flow_coeff = 0.6
 work_coeff = 0.4
 u_max = 450 #m/s
-Nstages = 10
-
-
+Nstages = 8
+RPM = 13800 #rot/min
+V1 = 150.38
+V2 = 206.68
+mdot_engine = 12.947804546087896
 
 t_t2 = t_t1 * beta_tt**((gamma_air-1)/gamma_air)
 work = cp_a*(t_t2 - t_t1)
 work_per_stage = work / Nstages
 tip_speed = np.sqrt(work_per_stage / work_coeff)
 deltaT = work_per_stage / cp_a
-t_tout1 = t_t1 + deltaT
-t_tratio1 = t_tout1 / t_t1
-beta_stage1 = (t_tratio1)**((gamma_air*comb_eff)/(gamma_air-1))
+
+T_lst = [t_t1]
+P_lst = [p_t1]
+Ps_lst = []
+Ts_lst = []
+M_lst = []
+T_ratio_lst = []
+Psratio_lst = []
+beta_lst = []
+x_axis = np.arange(0,Nstages,1) #delete *2 to plot pressure ratio per stage
+for i in range(Nstages):
+    Tt = T_lst[i] + deltaT
+    T_lst.append(Tt)
+    Ts = Tt - ((gamma_air - 1) / 2) * (V1 ** 2 / (gamma_air * R))
+    Ts_lst.append(Ts)
+    M = V1 / (np.sqrt(gamma_air * R * Ts_lst[i]))
+    M_lst.append(M)
+    Ttratio = Tt/T_lst[0]
+    T_ratio_lst.append(Ttratio)
+#    T_ratio_lst.append(Ttratio)
+    beta = Ttratio**((gamma_air*comb_eff)/(gamma_air-1))
+    beta_lst.append(beta)
+#    beta_lst.append(beta)
+    Pt = P_lst[i] * beta_lst[i]
+    P_lst.append(Pt)
+    Ps = P_lst[i]*(1+(gamma_air-1)/2*M**2)**(-gamma_air/(gamma_air-1))
+    Ps_lst.append(Ps)
+    Psratio = Ps_lst[i] / P_lst[0]
+    Psratio_lst.append(Psratio)
+ #   Psratio_lst.append(Psratio)
+
+
+mean_radius = tip_speed / ((2*np.pi*RPM)/60)
+blade_height = mdot_engine / (rho_amb * V1 * 2 * np.pi * mean_radius)
 print(work)
 print(tip_speed)
-print(t_tratio1)
-print(beta_stage1)
+print(mean_radius)
+print(blade_height)
+print(beta_lst)
+#plt.plot(x_axis[1:], T_ratio_lst[1:])
+#plt.plot(x_axis[1:], beta_lst[1:])
+plt.plot(x_axis[1:], Psratio_lst[1:])
+plt.show()
